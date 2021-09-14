f8fda35437644b75bfa87db5888430b75a21b182
Koyel Nandi <mr.admin@Koyels-MacBook-Air.local>
2021-09-14 08:22:55 -0600

    Sample coding for useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const nameRef = useRef();
+  const ageRef = useRef();
+  const marriedRef = useRef();
+  const submitRef = useRef();
+
+  useEffect(()=> {
+    nameRef.current.focus();
+  },[])
+
+  function submitHandle (){
+    alert("Form submitted");
+  }
+
+  function onKeyPressHandle(e) {
+    if(e.target.id === "nameInput"){
+      if(e.code === "Enter"){
+        ageRef.current.focus();
+      }
+    }
+
+    if(e.target.id === "ageInput"){
+      if(e.code === "Enter"){
+        marriedRef.current.focus();
+      }
+    }
+
+    if(e.target.id === "marriedInput"){
+      if(e.target.disabled === true){
+        e.target.disabled = false;
+      }
+      if(e.code === "Enter"){
+        submitRef.current.focus();
+      }
+    }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-field"> 
+        <span>Name</span>
+        <input id="nameInput" ref={nameRef} type="text" onKeyPress={onKeyPressHandle}></input>
+      </div>
+      <div className="form-field">
+        <span>Age</span>
+        <input id="ageInput" ref={ageRef} type="text" onKeyPress={onKeyPressHandle}></input>
+      </div>
+      <div className="form-field">
+        <span>Married?</span>
+        <input id="marriedInput" ref={marriedRef} type="checkbox" onKeyPress={onKeyPressHandle} onChange={onKeyPressHandle}></input>
+      </div>
+      <button id="submitBtn" ref={submitRef} onClick={submitHandle}>Submit</button>
     </div>
   );
 }
